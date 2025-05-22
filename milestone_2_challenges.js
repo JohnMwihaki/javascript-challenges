4275eb9cc2783903c66b2f4f3d904131033df1ae
john kimani <kimanimw21@gmail.com>
2025-05-22 22:50:29 +0300

    completed challenge 6 filter string by length

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -13,75 +13,67 @@ console.log(sumOfPositives([1, -3, -4, -6, 3]));
 
 //2.finding the maximam value in an array
 
-function findMax(array){
-    let great=[0]
-    let i=0;
-    while(i< array.length)
-    {
-        if(array[i]>great)
-        {
-            great=array[i]
-        }
-        i++;
+function findMax(array) {
+  let great = [0];
+  let i = 0;
+  while (i < array.length) {
+    if (array[i] > great) {
+      great = array[i];
     }
-    return great;
+    i++;
+  }
+  return great;
 }
-let list=[7,8,9,10];
-console.log(findMax(list))
+let list = [7, 8, 9, 10];
+console.log(findMax(list));
 
-//3.election winner 
-function findWinner(arrayOfOject){
-    let max=[0];
-    let i=0;
-    while(i<arrayOfOject.length){
-        if(arrayOfOject[i].votes>max.votes){
-            max=arrayOfOject[i]
-        }
-        i++;
+//3.election winner
+function findWinner(arrayOfOject) {
+  let max = [0];
+  let i = 0;
+  while (i < arrayOfOject.length) {
+    if (arrayOfOject[i].votes > max.votes) {
+      max = arrayOfOject[i];
     }
+    i++;
+  }
 
-    return max;
-
+  return max;
 }
-let candidate= [
+let candidate = [
   { name: "Alice", votes: 50 },
   { name: "Bob", votes: 75 },
-  { name: "Charlie", votes: 65 }
+  { name: "Charlie", votes: 65 },
 ];
 
-console.log(findWinner(candidate))
-
+console.log(findWinner(candidate));
 
 // 4.Longest word
-function findLongestWord(array){
-  let word=array[0];
-  let wrd=0;
+function findLongestWord(array) {
+  let word = array[0];
+  let wrd = 0;
 
-
-  for(wrd=0;wrd<array.length;wrd++)
-  {
-    if(array[wrd].length>=word.length )
-    {
-     word=array[wrd];
+  for (wrd = 0; wrd < array.length; wrd++) {
+    if (array[wrd].length >= word.length) {
+      word = array[wrd];
     }
-
   }
   return word;
-
 }
-let wordList=["mark","maina","lulu","ann"]
+let wordList = ["mark", "maina", "lulu", "ann"];
 console.log(findLongestWord(wordList));
 
 //5.Count Properties that takes a object and returns the numer of properties
 
-
 //6 Filter by length
 
-
-
+function filterByLength(arrayStrings, minLength) {
+  return arrayStrings.filter((s) => s.length >= minLength);
+}
+let words = ["cat", "giraffe", "hippo", "dog", "elephant"];
+console.log(filterByLength(words, 5));
 
 //7 sum of even numbers
-
 
 //8.Difference between sum of even and odd
 
@@ -97,7 +89,4 @@ console.log(findLongestWord(wordList));
 
 //14 cOunt Occurrences
 
-
 //15.Remove Duplicates
-
-
