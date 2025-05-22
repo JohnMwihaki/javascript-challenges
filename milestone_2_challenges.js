e4636d9901f7bd391ea21defb803544f9ee54335
john kimani <kimanimw21@gmail.com>
2025-05-22 20:00:57 +0300

    completed challange two find maximum value in array

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -1,15 +1,32 @@
 // 1. sum of possitive numbers
-function sumOfPositives(num){
-    let sum=0;
-    for(let i=0;i<=num.length;i++){
-        if(num[i]>0)
-        {
-            sum+=i
-        }
-
+function sumOfPositives(num) {
+  let sum = 0;
+  for (let i = 0; i <= num.length; i++) {
+    if (num[i] > 0) {
+      sum += i;
     }
-    
-return sum;
-}
-console.log(sumOfPositives([1,-3,-4,-6,3]))
+  }
 
+  return sum;
+}
+console.log(sumOfPositives([1, -3, -4, -6, 3]));
+
+//finding the maximam value in an array
+
+function findMax(array){
+    let great=[0]
+    let i=0;
+    while(i< array.length)
+    {
+        if(array[i]>great)
+        {
+            great=array[i]
+        }
+        i++;
+    }
+    return great;
+}
+let list=[7,8,9,10];
+console.log(findMax(list))
+
+//election winner 
